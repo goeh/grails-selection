6796f00e94342f3d79be7be0d92ca0290eeca347
Goran Ehrsson <goran@technipelago.se>
2013-10-25 21:32:04 +0200

    Added postalCode and city properties

--- grails-app/domain/test/TestEntity.groovy
+++ grails-app/domain/test/TestEntity.groovy
@@ -3,13 +3,17 @@ package test
 class TestEntity {
 
     String name
+    String postalCode
+    String city
     Integer age
 
     static constraints = {
         name(maxSize: 100)
+        postalCode(maxSize: 10, nullable: true)
+        city(maxSize: 50, nullable: true)
         age(nullable:true)
     }
-    
+
     String toString() {
         name
     }
