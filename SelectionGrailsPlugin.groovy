8427d11949da73cb312735858b0eb128f3d583dd
Goran Ehrsson <goran@technipelago.se>
2013-10-26 12:06:36 +0200

    Version 0.9.3

--- SelectionGrailsPlugin.groovy
+++ SelectionGrailsPlugin.groovy
@@ -21,7 +21,7 @@ import grails.plugins.selection.GrailsSelectionClass
 import org.codehaus.groovy.grails.web.servlet.mvc.GrailsParameterMap
 
 class SelectionGrailsPlugin {
-    def version = "0.9.3-SNAPSHOT"
+    def version = "0.9.3"
     def grailsVersion = "2.0 > *"
     def dependsOn = [:]
     def pluginExcludes = [
